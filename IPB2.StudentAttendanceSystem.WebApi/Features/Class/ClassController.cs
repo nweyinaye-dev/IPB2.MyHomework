f0e04918425cfcee11cb836545aaad0b34c8bd98
nweyinaye-11051997 <nweyinaye19970511@gmail.com>
2026-03-10 00:02:32 -0700

    feat: add Class Controller

---
+++ IPB2.StudentAttendanceSystem.WebApi/Features/Class/ClassController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IPB2.StudentAttendanceSystem.WebApi.Features.Class
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassController : ControllerBase
+    {
+    }
+}
